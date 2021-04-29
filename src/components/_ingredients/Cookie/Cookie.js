452e21837f1d6eb0ddce7a831764efeb8c45dcf2
Colby Fayock <fayock@gmail.com>
2021-04-29 14:13:26 -0400

    removing cookie inputs

--- src/components/_ingredients/Cookie/Cookie.js
+++ src/components/_ingredients/Cookie/Cookie.js
@@ -1,72 +1,31 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import { Box, Flex, Heading, FormLabel, Button, Input, Stack, VisuallyHidden } from '@chakra-ui/react';
-
-
-import Form from '@components/Form';
-import FormRow from '@components/FormRow';
+import { Box, Heading } from '@chakra-ui/react';
 
 import styles from './Cookie.module.scss';
 
-const Cookie = ({ id, label, name, defaultValue }) => {
+const Cookie = ({ id, name, value, label }) => {
   const cookieConfig = { path: '/' };
 
-  const [cookies, setCookie, removeCookie] = useCookies([name]);
-
-  const [value, setValue] = useState(defaultValue);
+  const [cookies, setCookie] = useCookies([name]);
 
   if ( !id ) {
     id = name;
   }
 
-  function handleSetCookie() {
-    setCookie(name, value, cookieConfig);
-  }
-
-  function handleDeleteCookie() {
-    removeCookie(name, cookieConfig);
-  }
+  useEffect(() => {
+    setCookie(name, value, cookieConfig)
+  }, []);
 
   return (
     <Box as="div" id={id} className={styles.cookie} marginBottom={10}>
-
       <Heading as="h2" fontSize={22} marginBottom={5}>{ label }</Heading>
-
-      <Form>
-        <Flex align="flex-start">
-          <FormRow id="set-cookie">
-            <Heading as="h3" fontSize={16} marginBottom={5}>Manage Cookie</Heading>
-            <Stack spacing={3}>
-              <p>
-                <Stack spacing={3}>
-                  <VisuallyHidden>
-                    <FormLabel fontWeight="bold" >Change Cookie Value</FormLabel>
-                  </VisuallyHidden>
-                  <Input value={value} onChange={(e) => setValue(e.currentTarget.value)} />
-                </Stack>
-              </p>
-              <p>
-                <Button id={`${id}-cookie-set`} colorScheme="blue" onClick={handleSetCookie} marginRight={2}>
-                  Set Cookie
-                </Button>
-                <Button id={`${id}-cookie-delete`} colorScheme="blue" onClick={handleDeleteCookie}>
-                  Delete Cookie
-                </Button>
-              </p>
-            </Stack>
-          </FormRow>
-          <Box width={500} marginLeft={10}>
-            <Heading as="h3" fontSize={16} marginBottom={5}>Cookie Value</Heading>
-            <p>
-              Name: <span id={`${id}-cookie-name`}>{ name }</span>
-            </p>
-            <p>
-              Value: <span id={`${id}-cookie-value`}>{ cookies[name] }</span>
-            </p>
-          </Box>
-        </Flex>
-      </Form>
-
+      <p>
+        Name: <span id={`${id}-cookie-name`}>{ name }</span>
+      </p>
+      <p>
+        Default Value: <span id={`${id}-cookie-value`}>{ cookies[name] }</span>
+      </p>
     </Box>
   )
 }
